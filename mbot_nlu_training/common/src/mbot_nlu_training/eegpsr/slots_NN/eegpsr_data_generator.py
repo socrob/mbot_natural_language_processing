48eb43a872ef802df1cce93c8a2dba7305dfdc44
Mithun <mkinarullathil@isr.tecnico.ulisboa.pt>
2018-09-27 16:37:56 +0100

    [mbot_nlu_training] reduced number of iterations for data generation to reduce the use of memory

--- mbot_nlu_training/common/src/mbot_nlu_training/eegpsr/slots_NN/eegpsr_data_generator.py
+++ mbot_nlu_training/common/src/mbot_nlu_training/eegpsr/slots_NN/eegpsr_data_generator.py
@@ -319,16 +319,16 @@ if n_struct['take'][1]:
     tasks_take_.append(['hand the ' + objet + ' to ' + name + ' at the ' + location.replace('location', 'destination') for objet in objects_the for name in names for location in locations_at])
     tasks_take_.append(['hand over the ' + objet + ' to ' + name + ' at the ' + location.replace('location', 'destination') for objet in objects_the for name in names for location in locations_at])
 
-    tasks_take_.append(['bring to ' + name + ' at the ' + location.replace('location', 'destination') + ' the ' + objet + ' from the ' + location2.replace('location', 'source') for name in names for location in locations_at[:int(len(locations_at)/2)] for objet in objects_the for location2 in locations[:int(len(locations)/4)] if location!=location2])
-    tasks_take_.append(['give to ' + name + ' at the ' + location.replace('location', 'destination') + ' the ' + objet + ' from the ' + location2.replace('location', 'source') for name in names for location in locations_at[:int(len(locations_at)/2)] for objet in objects_the for location2 in locations[:int(len(locations)/4)] if location!=location2])
+    tasks_take_.append(['bring to ' + name + ' at the ' + location.replace('location', 'destination') + ' the ' + objet + ' from the ' + location2.replace('location', 'source') for name in names for location in locations_at[:int(len(locations_at)/2)] for objet in objects_the for location2 in locations[:int(len(locations)/8)] if location!=location2])
+    tasks_take_.append(['give to ' + name + ' at the ' + location.replace('location', 'destination') + ' the ' + objet + ' from the ' + location2.replace('location', 'source') for name in names for location in locations_at[:int(len(locations_at)/2)] for objet in objects_the for location2 in locations[:int(len(locations)/8)] if location!=location2])
 
     tasks_take_.append(['get the ' + objet + ' from the ' + location.replace('location', 'source') for objet in objects_the for location in locations])
     tasks_take_.append(['get the ' + objet + ' to the ' + location.replace('location', 'destination') for objet in objects_the for location in locations])
     tasks_take_.append(['take the ' + objet + ' from the ' + location.replace('location', 'source') for objet in objects_the for location in locations])
     tasks_take_.append(['retrieve the ' + objet + ' from the ' + location.replace('location', 'source') for objet in objects_the for location in locations])
 
-    tasks_take_.append(['get the ' + objet + ' from the ' + location.replace('location', 'source') + ' to the ' + location2.replace('location', 'destination') for objet in objects_the for location in locations[:int(len(locations)/4)] for location2 in locations[:int(len(locations)/4)] if location!=location2])
-    tasks_take_.append(['take the ' + objet + ' from the ' + location.replace('location', 'source') + ' to the ' + location2.replace('location', 'destination') for objet in objects_the for location in locations[:int(len(locations)/4)] for location2 in locations[:int(len(locations)/4)] if location!=location2])
+    tasks_take_.append(['get the ' + objet + ' from the ' + location.replace('location', 'source') + ' to the ' + location2.replace('location', 'destination') for objet in objects_the for location in locations[:int(len(locations)/8)] for location2 in locations[:int(len(locations)/8)] if location!=location2])
+    tasks_take_.append(['take the ' + objet + ' from the ' + location.replace('location', 'source') + ' to the ' + location2.replace('location', 'destination') for objet in objects_the for location in locations[:int(len(locations)/8)] for location2 in locations[:int(len(locations)/8)] if location!=location2])
 
     tasks_take_.append(['place the ' + objet + ' on the ' + location.replace('location', 'destination') for objet in objects_the for location in locations_on])
     tasks_take_.append(['place ' + objet + ' on the ' + location.replace('location', 'destination') for objet in objects_the for location in locations_on])
@@ -344,8 +344,8 @@ if n_struct['take'][1]:
     tasks_take_.append(['grasp the ' + objet + ' to the ' + location.replace('location', 'destination') for objet in objects for location in locations])
     tasks_take_.append(['pick up the ' + objet + ' to the ' + location.replace('location', 'destination') for objet in objects for location in locations])
 
-    tasks_take_.append(['grasp the ' + objet + ' from the ' + location.replace('location', 'source') + ' to the ' + location2.replace('location', 'destination') for objet in objects for location in locations[:int(len(locations)/4)] for location2 in locations[:int(len(locations)/4)] if location!=location2])
-    tasks_take_.append(['pick up the ' + objet + ' from the ' + location.replace('location', 'source') + ' to the ' + location2.replace('location', 'destination') for objet in objects for location in locations[:int(len(locations)/4)] for location2 in locations[:int(len(locations)/4)] if location!=location2])
+    tasks_take_.append(['grasp the ' + objet + ' from the ' + location.replace('location', 'source') + ' to the ' + location2.replace('location', 'destination') for objet in objects for location in locations[:int(len(locations)/8)] for location2 in locations[:int(len(locations)/8)] if location!=location2])
+    tasks_take_.append(['pick up the ' + objet + ' from the ' + location.replace('location', 'source') + ' to the ' + location2.replace('location', 'destination') for objet in objects for location in locations[:int(len(locations)/8)] for location2 in locations[:int(len(locations)/8)] if location!=location2])
 
 
     # resampling and appending individual structures
